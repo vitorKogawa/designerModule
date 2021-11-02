b1b1b2949ae7aa7c900868bf3e1a6592b9ddf2aa
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-11-02 18:50:41 -0300

    create list screen

---
+++ src/Screens/ListsScreen/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Sidebar } from '../components/Sidebar/Sidebar'
+
+const ListScreen: React.FC = () => {
+    return (
+        <div className="container-fluid bg-primary min-vh-100">
+            <div className="row">
+                <div className="col bg-surface min-vh-100">
+                    <Sidebar />
+                </div>
+
+                <div className="col-8 d-flex flex-row flex-wrap justify-content-center align-items-center p-0">
+                    <h1>Em desenvolvimento...</h1>
+                </div>
+
+                <div className="col bg-primary sticky-top min-vh-100 p-1">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { ListScreen }
