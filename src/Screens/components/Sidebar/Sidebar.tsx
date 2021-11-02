6745b295035d6a237c1896e03cdf7854925e7568
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-11-02 18:52:42 -0300

    create sidebar component

---
+++ src/Screens/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {
+    BsHouseDoor,
+    BsHash,
+    BsBarChartLine,
+    BsCheckCircle,
+    BsBookmarks,
+    BsJournalText,
+    BsPerson,
+    BsThreeDots,
+    BsPersonCircle
+} from 'react-icons/bs'
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { ISidebarTopic } from './components/SidebarTopic/interfaces/ISidebarTopic';
+import { ButtonSidebar } from './components/ButtonSidebar/ButtonSidebar'
+import { DropdownPersonSidebar } from './components/DropdownPersonSidebar/DropdownPersonSidebar'
+import { SidebarTopic } from './components/SidebarTopic/SidebarTopic';
+import papiroLogo from './../../../Assets/img/papiro1.png';
+import './styles/styles.scss'
+
+const Sidebar: React.FC = () => {
+    const splitLocation = useLocation().pathname.split(",");
+    const sidebarTopics: ISidebarTopic[] = [
+        {
+            title: 'Home',
+            icon: <BsHouseDoor />,
+            path: '/home'
+        },
+        {
+            title: 'Explore',
+            icon: <BsHash />,
+            path: '/explore'
+        },
+        {
+            title: 'Analytics',
+            icon: <BsBarChartLine />,
+            path: '/analytics'
+        },
+        {
+            title: 'Quizzes',
+            icon: <BsCheckCircle />,
+            path: '/quizzes'
+        },
+        {
+            title: 'Library',
+            icon: <BsBookmarks />,
+            path: '/library'
+        },
+        {
+            title: 'Lists',
+            icon: <BsJournalText />,
+            path: '/lists'
+        },
+        {
+            title: 'Profile',
+            icon: <BsPerson />,
+            path: '/profile'
+        }
+    ];
+
+    return (
+        <nav className="nav d-flex flex-column justify-content-between min-vh-100 sticky-top p-1">
+            <div>
+                <Link to="/home" className="navbar-brand">
+                    <img src={papiroLogo} alt="Papiro logo" />
+                </Link>
+                {
+                    sidebarTopics.map((topic: ISidebarTopic) =>
+                        <SidebarTopic
+                            icon={topic.icon}
+                            path={topic.path}
+                            title={topic.title}
+                            isActive={splitLocation[0] === topic.path ? true : false}
+                        />
+                    )
+                }
+                <li className="nav-item d-flex align-items-center">
+                    <BsThreeDots />
+                    <Link to="#" className="nav-link">
+                        More
+                    </Link>
+                </li>
+                <ButtonSidebar label="New Game" />
+            </div>
+
+            <DropdownPersonSidebar />
+        </nav>
+    )
+}
+
+export { Sidebar }
