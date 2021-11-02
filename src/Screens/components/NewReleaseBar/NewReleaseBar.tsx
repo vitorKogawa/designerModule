cace1c28d54020aaa92cc7edc54ec9e2beef4b83
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-11-02 18:54:36 -0300

    create newReleaseBar component

---
+++ src/Screens/components/NewReleaseBar/NewReleaseBar.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { NewReleaseBarItem } from './components/NewReleaseBarItem/NewReleaseBarItem'
+import { NewReleaseSearchInput } from './components/NewReleaseSearchInput/NewReleaseSearchInput'
+
+const NewReleaseBar: React.FC = () => {
+    const newReleasesItens: { title: string, content: string }[] = [
+        {
+            title: 'Release Item 1',
+            content: ''
+        },
+        {
+            title: 'Release Item 1',
+            content: ''
+        },
+        {
+            title: 'Release Item 1',
+            content: ''
+        },
+        {
+            title: 'Release Item 1',
+            content: ''
+        },
+        {
+            title: 'Release Item 1',
+            content: ''
+        },
+    ]
+
+    return (
+        <React.Fragment>
+            <NewReleaseSearchInput />
+            <div className="accordion" id="accordionExample1">
+                <h3>New Releases</h3>
+                {
+                    newReleasesItens.map((item: { title: string, content: string }, index: number) =>
+                        <NewReleaseBarItem
+                            title={item.title}
+                            content={item.content}
+                            index={index}
+                        />
+                    )
+                }
+                <h6>Show more</h6>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export { NewReleaseBar }
