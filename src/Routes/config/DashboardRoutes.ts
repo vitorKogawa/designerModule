aa27cb72f07be24deb4038d8cf55975ac0cf982c
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-11-15 10:28:58 -0300

    add build-game route in dashboardRoutes

--- src/Routes/config/DashboardRoutes.ts
+++ src/Routes/config/DashboardRoutes.ts
@@ -1,6 +1,7 @@
 import { AnalyticsScreen } from "../../Screens/AnalyticsScreen"
 import { ExploreScreen } from "../../Screens/ExploreScreen"
 import { HomeScreen } from "../../Screens/HomeScreen"
+import { BuildingGame } from "../../Screens/HomeScreen/pages/BuildingGame"
 import { LibraryScreen } from "../../Screens/LibraryScreen"
 import { ListScreen } from "../../Screens/ListsScreen"
 import { ProfileScreen } from "../../Screens/ProfileScreen"
@@ -35,6 +36,10 @@ const DASHBOARD_ROUTES: IDashboardRoutes[] = [
     {
         pathRoute: '/profile',
         componentRoute: ProfileScreen
+    },
+    {
+        pathRoute: '/build-game',
+        componentRoute: BuildingGame
     }
 ]
 
