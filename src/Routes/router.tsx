697c7d62d50d17839d94928ca8c9c6ddf1c35c41
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-01-13 17:43:52 -0300

    remove dashboard route and add home route

--- src/Routes/router.tsx
+++ src/Routes/router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from '../Screens/Dashboard';
+import { HomeScreen } from '../Screens/HomeScreen/HomeScreen'; //homepage
 import SignInScreen from '../Screens/SignInScreen';
 import PrivateRoute from './PrivateRoute';
 import { AuthProvider } from "./Auth";
@@ -16,7 +16,7 @@ const Router:React.FC = () => {
             <BrowserRouter >
                 <Switch>
                     <Route exact path="/" component={SignInScreen} />
-                    <PrivateRoute exact path="/dashboard" component={App} />
+                    <PrivateRoute exact path="/home" component={HomeScreen} />
                     <PrivateRoute exact path="/editor" component={EditorScreen} />
                     <PrivateRoute exact path="/play" component={Play} />
                     {
