e9b693b3ee361cc36ca2f5b888ad3dbb9585f4eb
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-09-20 22:49:56 -0300

    ajustando componente de nó comum

--- src/Screens/HomeScreen/pages/BuildingGame/components/Flow/components/Nodes/CommonNode/CommonNode.tsx
+++ src/Screens/HomeScreen/pages/BuildingGame/components/Flow/components/Nodes/CommonNode/CommonNode.tsx
@@ -1,22 +1,20 @@
 import React, { memo, FC, CSSProperties, Fragment } from 'react';
 import { Handle, Position, NodeProps, Connection, Edge } from 'react-flow-renderer';
+import { EditNodeModal } from '../../../../EditNodeModal/EditNodeModal';
 // import { BsLightning } from 'react-icons/bs'
 import './../styles/style.scss'
 
-const targetHandleStyle: CSSProperties = { background: '#555' };
-const sourceHandleStyleA: CSSProperties = { ...targetHandleStyle, top: 10 };
-const sourceHandleStyleB: CSSProperties = { ...targetHandleStyle, bottom: 10, top: 'auto' };
-
-const onConnect = (params: Connection | Edge) => console.log('handle onConnect', params);
-
-const CommonNode: FC<NodeProps> = ({ data, isConnectable }) => {
+const CommonNode: FC<{ id: number }> = (props) => {
 
     return (
-        <div key={data.id}>
-            <Handle type="target" position={Position.Left} style={targetHandleStyle} onConnect={onConnect} />
+        <div key={props.id}>
+            <Handle type='target' id='a' position={Position.Left} />
+            <Handle type='source' id='b' position={Position.Right} />
+            <Handle type='source' id='c' position={Position.Right} />
             <div className="my-card">
                 <div className="my-card-header">
                     {/* <BsLightning/> */}
+                    <EditNodeModal id={props.id} />
                 </div>
                 <div className="my-card-body">
                     <p className="my-card-title">
@@ -29,17 +27,6 @@ const CommonNode: FC<NodeProps> = ({ data, isConnectable }) => {
                     <div className="my-card-footer-item pallet-3"></div>
                 </div>
             </div>
-            <Handle
-                type="source"
-                position={Position.Right}
-                id="a"
-                style={sourceHandleStyleA}
-                isConnectable={isConnectable}
-                onMouseDown={(e) => {
-                    console.log('You trigger mousedown event', e);
-                }}
-            />
-            <Handle type="source" position={Position.Right} id="b" style={sourceHandleStyleB} isConnectable={isConnectable} />
         </div>
     );
 };
