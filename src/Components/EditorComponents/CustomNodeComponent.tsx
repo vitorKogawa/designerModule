ae2ce5fac68223b050014a3c43447ab516457caa
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-09 18:59:18 -0300

    ajustes no custom node component

--- src/Components/EditorComponents/CustomNodeComponent.tsx
+++ src/Components/EditorComponents/CustomNodeComponent.tsx
@@ -1,34 +1,30 @@
-import React, { Fragment, useState } from 'react';
-import { BsLightning, BsRecordCircle } from 'react-icons/bs'
+import React, { useState } from 'react'
 import { Position, Handle } from 'react-flow-renderer';
-import { EditNodeModal } from '../../Screens/HomeScreen/pages/BuildingGame/components/EditNodeModal/EditNodeModal';
-// import './EditorComponentsStyles/CustomNodeComponent.scss'
+import './EditorComponentsStyles/CustomNodeComponent.css';
 
-const CustomNode: React.FC<{ id: number }> = (props) => {
+function CustomNodeComponent({ data }: any){
   return (
-    <Fragment>
-      <div className="card p-0 w-100 border-0 m-0">
-        <Handle type='target' id='a' position={Position.Left} />
-        <Handle type='source' id='b' position={Position.Right} />
-        <Handle type='source' id='c' position={Position.Right} /> 
-        {/* <img src="..." className="card-img-top" alt="..." /> */}
-        <div className="card-body p-0">
-            <button className="btn btn-primary w-25 h-100 p-0">
-              <BsLightning/>
-            </button>
-            <h5 className="card-title my-1">Card title</h5>
-          {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-          {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
-          <button className="btn btn-pallet-1 w-20 h-100 rounded-pill mx-1"></button>
-          <button className="btn btn-pallet-2 w-20 h-100 rounded-pill mx-1"></button>
-          <button className="btn btn-pallet-3 w-20 h-100 rounded-pill mx-1"></button>
-          <EditNodeModal id={props.id} />
+    <div className="customNodeContainer">
+        <Handle type='target' id='a' position={Position.Left} style={{border: 0, background: 'rgba(0,0,0,0.0)', width: 10, height: 10, borderRadius: '50%' }} />
+        <Handle type='source' id='b' position={Position.Right} style={{border: 0, background: 'rgba(0,0,0,0.0)', width: 10, height: 10, top: '40%', borderRadius: '50%' }} />
+        <Handle type='source' id='c' position={Position.Right} style={{border: 0, background: 'rgba(0,0,0,0.0)', width: 10, height: 10, top: '60%', borderRadius: '50%' }} />
+      <div className="body_container">
+        <label className="title">{data.title? data.title : 'Cartão sem nome'}</label>
+        <div className="tags_container">
+          {data.tagsArray.map((element: any, i:any) => {
+            return (<span style={{backgroundColor: element.color}} className="tag" key={i}>{element.name}</span>)
+          })}
         </div>
+        <span>Descrição - Clique em editar.</span>
       </div>
+      <div className="edit_container">
+        <div className="edit_button" onClick={data.onEditClick}>
+          <span>editar</span>
+        </div> 
+      </div>
+      
+    </div>
+  );
+};
 
-      {/* <NodeModal id={props.id} /> */}
-    </Fragment>
-  )
-}
-
-export { CustomNode }
+export { CustomNodeComponent }
